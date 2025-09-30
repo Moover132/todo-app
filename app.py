4af81c1ddffa66b2ce43a79ca93a9e27381842da
Ivan Reznikov <ivan6097809@gmail.com>
2025-09-30 17:19:40 +0400

    creat db and upgrate html, add task list

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)  
+    complete = db.Column(db.Boolean, default=False) 
 
 @app.route('/')
 def index():
-    return render_template('base.html')
-
-
+    # Показать все задачи
+    todo_list = Todo.query.all()
+    print(todo_list)
+    return render_template('base.html', todos=todo_list) 
 
 if __name__ == "__main__":
+    with app.app_context(): 
+        db.create_all()       
     app.run(debug=True)
