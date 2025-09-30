9851681329507151d4f82dd5e2d30b231fd755ba
Ivan Reznikov <ivan6097809@gmail.com>
2025-09-30 17:50:02 +0400

    add delet and update, fix neponyatnaya huina sps indusu kotori pomog

--- app.py
+++ app.py
@@ -18,10 +18,26 @@ def index():
     print(todo_list)
     return render_template('base.html', todos=todo_list) 
 @app.route("/add",methods=["POST"])
+
+@app.route("/add", methods=["POST"])
 def add():
     title = request.form.get("title")
     new_todo = Todo(title=title, complete=False)
     db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
     db.session.commit()
     return redirect(url_for("index"))
     
